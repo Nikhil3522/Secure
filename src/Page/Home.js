e7c5d5748c06169df78a17c0afbf1dc43ae91865
Nikhil3522 <nikhiljha3522@gmail.com>
2023-04-29 23:33:25 +0530

    Home page style

--- src/Page/Home.js
+++ src/Page/Home.js
@@ -3,6 +3,7 @@ import lottie from "lottie-web";
 import Lottie from "react-lottie";
 import animationData from '../assest/93112-security.json';
 import { useNavigate } from "react-router-dom";
+import { Typewriter } from 'react-simple-typewriter';
 
 const Home = () => {
     const navigate = useNavigate();
@@ -15,87 +16,114 @@ const Home = () => {
         }
       };
     return (
-        <div className="flex flex-wrap">
-            <Lottie 
-                options={defaultOptions}
-                height={400}
-                width={400}
-            />
-            <div className="container" ></div>
-            <div 
-                className="bg-blue-400 cursor-pointer w-[280px] h-[220px] p-4 m-4"
-                onClick={() => navigate('/encryption')}
-            >
-                <h3 className="text-3xl font-bold text-white text-center">Encryption</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
+        <div className="flex flex-col">
+            <div className="flex m-[auto] w-[800px] ">
+                <h1 className="text-4xl  mt-[160px] min-w-[400px]">
+                    <Typewriter
+                        words={['Secure your digital world with our encryption solutions',
+                                'Protect your data, secure your privacy',
+                                'Say goodbye to weak passwords',
+                                'Your data, your control',
+                                'Empowering you with the tools to stay safe',
+                                'Privacy in a digital age',
+                                'Encrypt your messages, protect your privacy',
+                                'Secure your online life with ease'
+                            ]}
+                        cursor
+                        cursorStyle='|'
+                        typeSpeed={40}
+                        deleteSpeed={0}
+                        loop={0}
+                    />
+                </h1>
+                
+                <div className="container " >
+                    <Lottie 
+                        options={defaultOptions}
+                        height={400}
+                        width={400}
+                    />
+                </div>
+            </div>
+            <div className="flex flex-wrap m-[auto] justify-center">
+                <div 
+                    className="bg-blue-400 cursor-pointer w-[280px] h-[220px] p-4 m-4"
+                    onClick={() => navigate('/encryption')}
+                >
+                    <h3 className="text-3xl font-bold text-white text-center">Encryption</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">decryption</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">Password Generator</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">Password Strength Checker</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">Password Manager</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">Access request</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">Documents Manager</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
+                <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
+                    <h3 className="text-3xl font-bold text-white text-center">History</h3>
+                    <ol>
+                        <li>Text</li>
+                        <li>Image</li>
+                        <li>File</li>
+                    </ol>
+                </div>
+
             </div>
 
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">decryption</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">Password Generator</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">Password Strength Checker</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">Password Manager</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">Access request</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">Documents Manager</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
-
-            <div className="bg-blue-400 w-[280px] h-[220px] p-4 m-4">
-                <h3 className="text-3xl font-bold text-white text-center">History</h3>
-                <ol>
-                    <li>Text</li>
-                    <li>Image</li>
-                    <li>File</li>
-                </ol>
-            </div>
+            
         </div>
     )
 } 
